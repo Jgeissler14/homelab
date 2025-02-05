fa17dec1869e8dbe15e450440934abc48d146c86
Joshua Geissler <jgeissler14@gmail.com>
2025-02-04 16:34:59 -0800

    100 max pods

--- load_testing.js
+++ load_testing.js
@@ -4,13 +4,13 @@ import { sleep } from 'k6';
 export const options = {
   vus: 100,
   stages: [
-    { duration: '1m', target: 100 },
-    { duration: '3m', target: 500 },
+    { duration: '1m', target: 1000 },
+    { duration: '3m', target: 5000 },
     { duration: '1m', target: 0 },
   ],
 };
 
 export default function () {
-  http.get('http://my-website.com');
+  http.get('http://20.120.147.230');
   sleep(0.1);
 }
