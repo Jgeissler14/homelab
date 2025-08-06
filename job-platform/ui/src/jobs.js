194711f6958f3b8fa53df426382f260882991620
Joshua Geissler <jgeissler14@gmail.com>
2025-08-06 13:51:15 -0700

    fix

---
+++ job-platform/ui/src/jobs.js
@@ -0,0 +1,36 @@
+
+export const jobs = [
+  {
+    id: 'ubuntu-sleep',
+    name: 'Ubuntu Sleep',
+    description: 'A simple job that sleeps for 30 seconds.',
+    template: 'my-job',
+    params: {
+      container_name: 'ubuntu',
+      image: 'ubuntu:latest',
+      command: 'sleep 30',
+    },
+  },
+  {
+    id: 'alpine-wget',
+    name: 'Alpine Wget',
+    description: 'A simple job that uses wget to download a file.',
+    template: 'my-job',
+    params: {
+      container_name: 'alpine',
+      image: 'alpine:latest',
+      command: 'wget -q -O - https://example.com',
+    },
+  },
+  {
+    id: 'busybox-echo',
+    name: 'Busybox Echo',
+    description: 'A simple job that echoes a message.',
+    template: 'my-job',
+    params: {
+      container_name: 'busybox',
+      image: 'busybox:latest',
+      command: 'echo "Hello, from Busybox!"',
+    },
+  },
+];
